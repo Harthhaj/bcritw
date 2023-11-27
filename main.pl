533193ec1afc8b0cce0c05d55a2198cab56a8298
Ez Aldin Waez <ez.aldin.waez@gmail.com>
2023-11-27 12:38:47 +0300

    edit Mjadarah ingredients

--- main.pl
+++ main.pl
@@ -6,7 +6,7 @@ meals([
     ['Mehshi', ['Spices', 'Meat', 'Vegetables', 'Rice']],
     ['Mulukhiyah', ['Spices', 'Meat', 'Garlic', 'Mulukhiyah Leaves']],
     ['Fasolia', ['Spices', 'Meat', 'Tomato Souce', 'Garlic', 'Beans']],
-    ['Mjadarah', ['Onion', 'Bulgur', 'Lentil']],
+    ['Mjadarah', ['Onion', 'Rice', 'Lentil']],
     ['Spaghetti', ['Spices', 'Tomato Souce', 'Vegetables', 'Macaroni']],
     ['Yabraq', ['Spices', 'Meat', 'Garlic', 'Rice', 'Grape Leaves']],
     ['Orman-Blaban', ['Spices', 'Meat', 'Butter', 'Yogurt', 'Corn Starch', 'Egges']]
