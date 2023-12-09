87cf2fc2443a34e599dfffd315271b79023022c0
Ez Aldin Waez <ez.aldin.waez@gmail.com>
2023-12-10 02:05:54 +0300

    refactoring the project; add third elem for meals

--- main.pl
+++ main.pl
@@ -1,16 +1,4 @@
-meals([
-    ['Labania', ['Yogurt','Rice','Meat','Spices','Garlic', 'Egges']],
-    ['Kebbah', ['Spices', 'Meat', 'Onion', 'Bulgur']],
-    ['Bamia', ['Spices', 'Meat', 'Tomato Souce', 'Okra', 'Bread']],
-    ['Safargeliah', ['Spices', 'Meat', 'Tomato Souce', 'Quince']],
-    ['Mehshi', ['Spices', 'Meat', 'Vegetables', 'Rice']],
-    ['Mulukhiyah', ['Spices', 'Meat', 'Garlic', 'Mulukhiyah Leaves']],
-    ['Fasolia', ['Spices', 'Meat', 'Tomato Souce', 'Garlic', 'Beans']],
-    ['Mjadarah', ['Onion', 'Rice', 'Lentil']],
-    ['Spaghetti', ['Spices', 'Tomato Souce', 'Vegetables', 'Macaroni']],
-    ['Yabraq', ['Spices', 'Meat', 'Garlic', 'Rice', 'Grape Leaves']],
-    ['Orman-Blaban', ['Spices', 'Meat', 'Butter', 'Yogurt', 'Corn Starch', 'Egges']]
-]).
+:- include('meals.pl').
 
 run :-
     print_welcome,
@@ -20,9 +8,9 @@ run :-
 
 
 print_welcome :-
-    write('*******************'), nl,
-    write('*   B-C-R-I-T-W   *'), nl,
-    write('*******************'), nl,
+    write('*****************************'), nl,
+    write('*        B-C-R-I-T-W        *'), nl,
+    write('*****************************'), nl,
     nl.
 
 
@@ -33,10 +21,13 @@ check_meals([H|T]) :-
     check_meal(H),
     check_meals(T).
 
-check_meal([MealName, Ingredients]) :-
+check_meal([MealName, Ingredients, HowToPrepare]) :-
     check_ingredients(Ingredients),
     nl,
     write('* You can cook '), write(MealName), write(' *'), nl,
+    write('To cook that, follow the instructions:'), nl,
+    write(HowToPrepare), nl,
+    nl,
     ask_to_complete.
 
 check_meal(_). % it will always return `true`, even if `check_ingredients` returns `false`.
@@ -84,6 +75,8 @@ undo.
 exit :-
     undo,
     nl,
-    write("********** Thank you for using this App **********"),nl,
+    write('*****************************'), nl,
+    write('* Thanks For Using This App *'), nl,
+    write('*****************************'), nl,
     nl,
     halt.
---
+++ meals.pl
@@ -0,0 +1,57 @@
+meals([
+    [
+        'Labania',
+        ['Yogurt','Rice','Meat','Spices','Garlic', 'Egges'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Kebbah',
+        ['Spices', 'Meat', 'Onion', 'Bulgur'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Bamia',
+        ['Spices', 'Meat', 'Tomato Souce', 'Okra', 'Bread'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Safargeliah',
+        ['Spices', 'Meat', 'Tomato Souce', 'Quince'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Mehshi',
+        ['Spices', 'Meat', 'Vegetables', 'Rice'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Mulukhiyah',
+        ['Spices', 'Meat', 'Garlic', 'Mulukhiyah Leaves'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Fasolia',
+        ['Spices', 'Meat', 'Tomato Souce', 'Garlic', 'Beans'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Mjadarah',
+        ['Onion', 'Rice', 'Lentil'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Spaghetti',
+        ['Spices', 'Tomato Souce', 'Vegetables', 'Macaroni'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Yabraq',
+        ['Spices', 'Meat', 'Garlic', 'Rice', 'Grape Leaves'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ],
+    [
+        'Orman-Blaban',
+        ['Spices', 'Meat', 'Butter', 'Yogurt', 'Corn Starch', 'Egges'],
+        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+    ]
+]).
