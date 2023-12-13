6bfa9584e144ad89363e58e89bca390e8e2d4c22
Harthhaj <harthhaj446@gmail.com>
2023-12-13 22:45:06 +0300

    Adding how to prepare each meal .

--- meals.pl
+++ meals.pl
@@ -1,57 +1,182 @@
 meals([
     [
-        'Labania',
-        ['Yogurt','Rice','Meat','Spices','Garlic', 'Egges'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        'Labneh', 
+        ['Full-fat yogurt','Cheesecloth or clean kitchen towel','Salt to taste',
+        'Olive oil (optional)','Dried mint or herbs (optional)'],
+
+        '1. Start with fresh, full-fat yogurt. You can use homemade or store-bought yogurt.\n',
+        '2. Place a large piece of cheesecloth or a clean kitchen towel in a strainer over a bowl.\n',
+        '3. Pour the yogurt onto the cheesecloth, gathering the corners and tying them together to create a bundle.\n',
+        '4. Hang the yogurt bundle over the sink or a large bowl to allow the whey to drain for 12-24 hours.\n',
+        '5. Once the labneh reaches the desired thickness, transfer it to a bowl.\n',
+        '6. Season the labneh with salt to taste and mix well.\n',
+        '7. If you prefer, add a drizzle of olive oil and sprinkle with dried mint or your favorite herbs.\n',
+        '8. Refrigerate the labneh for a few hours to enhance the flavors.\n',
+        '9. Serve the Syrian Labneh (Labaniyyeh Halabia) with flatbread, vegetables, or as a dip.\n',
+        '10. Enjoy your homemade Labneh!'
     ],
     [
         'Kebbah',
-        ['Spices', 'Meat', 'Onion', 'Bulgur'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        ['Ground meat (beef or lamb)','Cracked wheat (burghul)','Onions',
+        'Pine nuts','Allspice','Cinnamon','Salt','Pepper','Olive oil','Water'],
+        
+        '1. Soak cracked wheat (burghul) in water for 30 minutes, then drain well.\n',
+        '2. In a food processor, combine soaked cracked wheat, minced meat (beef or lamb), and onions.\n',
+        '3. Process the mixture until it forms a smooth paste.\n',
+        '4. Add spices such as ground allspice, cinnamon, salt, and pepper to the mixture and blend again.\n',
+        '5. In a separate bowl, combine finely chopped onions and pine nuts for the stuffing.\n',
+        '6. Wet your hands, take a portion of the kibbeh mixture, and shape it into a ball.\n',
+        '7. Make a hole in the ball and stuff it with the onion and pine nut mixture.\n',
+        '8. Seal the ball, shaping it into an oval or football-like form.\n',
+        '9. Repeat the process until all the kibbeh is formed.\n',
+        '10. In a deep pan, heat vegetable oil for frying.\n',
+        '11. Carefully place the kibbeh in the hot oil and fry until golden brown and crispy.\n',
+        '12. Remove the kibbeh from the oil and place them on paper towels to absorb excess oil.\n',
+        '13. Serve the Syrian Kibbeh (Kubba Halabia) with your favorite dipping sauce or yogurt.'
     ],
     [
         'Bamia',
-        ['Spices', 'Meat', 'Tomato Souce', 'Okra', 'Bread'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        ['Okra','Meat (chicken, beef, or lamb)','Tomato Sauce','Tomatoes',
+        'Garlic','Onion','Olive Oil','Spices (cumin, coriander, paprika)',
+        'Salt and Pepper to taste'],
+
+        '1. Wash and cut the okra into small pieces.\n',
+        '2. In a pot, heat some oil and sauté chopped onions until golden brown.\n',
+        '3. Add crushed garlic to the onions and sauté for a minute.\n',
+        '4. Stir in the spices and continue to cook for a couple of minutes.\n',
+        '5. Add tomato sauce to the mixture and cook until it thickens.\n',
+        '6. In a separate pan, lightly fry the okra until it is no longer slimy.\n',
+        '7. Add the fried okra to the pot and mix well with the sauce.\n',
+        '8. Pour enough water to cover the ingredients and bring to a boil.\n',
+        '9. Reduce the heat, cover the pot, and let it simmer until the okra is tender.\n',
+        '10. Season with salt and pepper to taste.\n',
+        '11. Serve the Syrian Okra Stew (Bamia) over rice or with your preferred side.\n'
     ],
     [
-        'Safargeliah',
-        ['Spices', 'Meat', 'Tomato Souce', 'Quince'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        'Safargliyya',
+        ['Quince','Meat (beef or lamb)','Tomato Sauce','Garlic',
+        'Spices (cinnamon, allspice, salt, pepper)','Water'],
+
+        '1. Peel and slice quince into small pieces.\n',
+        '2. In a pot, cook the quince with water until it becomes tender.\n',
+        '3. In a separate pan, sauté meat with spices until browned.\n',
+        '4. Add tomato sauce to the meat and cook until the sauce thickens.\n',
+        '5. Mix the cooked quince with the meat and tomato sauce.\n',
+        '6. Add more water if needed and let it simmer until the flavors blend.\n',
+        '7. In a small pan, sauté garlic until golden, then add it to the pot.\n',
+        '8. Season with salt and pepper to taste.\n',
+        '9. Simmer until the Safargliyya reaches the desired consistency.\n',
+        '10. Serve the Syrian Safargliyya (Safarjaliyya Halabia) over rice or with flatbread.\n',
+        '11. Enjoy this flavorful Syrian dish!'
     ],
     [
-        'Mehshi',
-        ['Spices', 'Meat', 'Vegetables', 'Rice'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        'Mahshi',
+        ['Assorted vegetables for stuffing (zucchini, eggplants, grape leaves, etc.)',
+        'Ground meat (beef or lamb)','Rice','Chopped onions','Spices (allspice, cinnamon, salt, pepper)',
+        'Tomato sauce','Water','Garlic','Olive oil'],
+
+        '1. Select your preferred vegetables for stuffing, such as zucchini, eggplants, or grape leaves.\n',
+        '2. Wash the vegetables thoroughly and prepare them for stuffing by removing seeds or hollowing them.\n',
+        '3. In a bowl, mix ground meat (beef or lamb) with rice, chopped onions, spices, and herbs.\n',
+        '4. Stuff each vegetable with the meat and rice mixture, ensuring they are tightly filled.\n',
+        '5. Arrange the stuffed vegetables in a pot, placing them close together to prevent unraveling.\n',
+        '6. In a separate bowl, mix tomato sauce, water, and additional spices to create a flavorful sauce.\n',
+        '7. Pour the sauce over the stuffed vegetables in the pot.\n',
+        '8. Place a heavy plate or lid on top of the stuffed vegetables to keep them in place during cooking.\n',
+        '9. Bring the pot to a gentle boil, then reduce the heat to simmer and cook until the rice is tender.\n',
+        '10. Check for doneness, and add more water if needed.\n',
+        '11. Once cooked, remove from heat and let it rest for a few minutes.\n',
+        '12. Serve the Syrian Mahshi hot, optionally with a side of yogurt or your favorite sauce.\n',
+        '13. Enjoy the delicious flavors of Syrian Stuffed Vegetables!'
     ],
     [
         'Mulukhiyah',
-        ['Spices', 'Meat', 'Garlic', 'Mulukhiyah Leaves'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        ['Mulukhiyah leaves','Meat (chicken, beef, or lamb)','Onions','Garlic',
+        'Olive oil','Spices (coriander, salt, pepper)','Water'],
+
+        '1. Wash the mulukhiyah leaves thoroughly and chop them finely.\n',
+        '2. In a pot, sauté chopped onions and garlic in olive oil until golden brown.\n',
+        '3. Add meat (chicken, beef, or lamb) to the pot and brown it on all sides.\n',
+        '4. Season the meat with ground coriander, salt, pepper, and any preferred spices.\n',
+        '5. Pour enough water to cover the meat and let it simmer until the meat is tender.\n',
+        '6. In a separate pan, lightly fry the chopped mulukhiyah leaves in a bit of oil.\n',
+        '7. Add the mulukhiyah to the pot with the cooked meat and stir well.\n',
+        '8. Let the stew simmer for an additional 15-20 minutes, allowing the flavors to meld.\n',
+        '9. In a small pan, sauté minced garlic until golden, then add it to the stew.\n',
+        '10. Adjust the seasoning as needed and simmer for a few more minutes.\n',
+        '11. Serve the Syrian Mulukhiyah hot over rice or with bread.\n',
+        '12. Enjoy the rich and flavorful Syrian Mulukhiyah Stew!'
     ],
     [
         'Fasolia',
-        ['Spices', 'Meat', 'Tomato Souce', 'Garlic', 'Beans'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        ['Dried white beans','Chopped onions','Minced garlic','Olive oil','Tomato sauce',
+        'Tomato paste','Spices (cumin, coriander, salt, pepper)','Water','Butter','Flour'],
+
+        '1. Wash and soak dried white beans overnight in water.\n',
+        '2. In a pot, cook the soaked beans in fresh water until they are tender.\n',
+        '3. In a separate pan, sauté chopped onions in olive oil until translucent.\n',
+        '4. Add minced garlic to the onions and sauté for an additional minute.\n',
+        '5. Stir in tomato sauce and tomato paste, cooking until the mixture thickens.\n',
+        '6. Add the cooked beans to the tomato mixture, along with spices like cumin, coriander, and salt.\n',
+        '7. Pour in enough water to cover the beans and let the stew simmer for about 20-30 minutes.\n',
+        '8. In a small pan, prepare a simple roux by melting butter and stirring in flour until it forms a paste.\n',
+        '9. Add the roux to the bean mixture, stirring well to incorporate and thicken the stew.\n',
+        '10. Adjust the seasoning as needed and let it simmer for a few more minutes.\n',
+        '11. Serve the Syrian Fasolia hot, either as a main dish or a side, with rice or bread.\n',
+        '12. Enjoy the hearty and flavorful Syrian White Bean Stew!'
     ],
     [
-        'Mjadarah',
-        ['Onion', 'Rice', 'Lentil'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        'Mujadara',
+        ['Lentils','Rice','Onions','Olive oil','Salt','Pepper','Ground cumin','Water',
+        'Optional: Yogurt for serving'],
+
+        '1. Rinse and soak lentils in water for about 30 minutes, then drain.\n',
+        '2. In a pot, combine soaked lentils, rice, and water. Bring to a boil, then simmer until both lentils and rice are cooked.\n',
+        '3. In a separate pan, caramelize thinly sliced onions in olive oil until golden brown and crispy.\n',
+        '4. Once the lentils and rice are cooked, mix them together in a serving dish.\n',
+        '5. Season with salt, pepper, and ground cumin to taste.\n',
+        '6. Top the mujadara with the caramelized onions, reserving some for garnish.\n',
+        '7. Optionally, serve with yogurt on the side.\n',
+        '8. Garnish with fresh chopped parsley or mint.\n',
+        '9. Drizzle with additional olive oil if desired.\n',
+        '10. Serve the Syrian Mujadara warm as a main dish or a side.\n',
+        '11. Enjoy this classic and flavorful Syrian dish!'
     ],
     [
-        'Spaghetti',
-        ['Spices', 'Tomato Souce', 'Vegetables', 'Macaroni'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        'Pasta',
+        ['Pasta','Onions','Assorted vegetables (bell peppers, zucchini, cherry tomatoes, etc.)','Garlic','Olive oil',
+        'Spices (salt, black pepper, and preferred herbs or spices)','Tomato sauce or diced tomatoes (optional)',
+        'Fresh herbs or grated cheese for garnish (optional)'],
+
+        '1. Boil water in a large pot and cook the pasta according to the package instructions.\n',
+        '2. In a pan, heat olive oil and sauté finely chopped onions until translucent.\n',
+        '3. Add a variety of your favorite vegetables, such as bell peppers, zucchini, and cherry tomatoes.\n',
+        '4. Stir in minced garlic and continue sautéing until the vegetables are tender-crisp.\n',
+        '5. Season the vegetables with salt, black pepper, and any preferred herbs or spices.\n',
+        '6. Optionally, you can add tomato sauce or diced tomatoes to create a saucy base.\n',
+        '7. Once the pasta is cooked, drain it and add it to the pan with the vegetables.\n',
+        '8. Toss the pasta and vegetables together until well combined.\n',
+        '9. Adjust the seasoning and drizzle with olive oil for extra flavor.\n',
+        '10. Serve the Syrian Vegetable Pasta hot, garnished with fresh herbs or grated cheese if desired.\n',
+        '11. Enjoy this simple and delicious Syrian pasta dish!'
     ],
     [
         'Yabraq',
-        ['Spices', 'Meat', 'Garlic', 'Rice', 'Grape Leaves'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
+        ['Grape leaves','Ground meat (beef or lamb)','Rice','Onions','Spices (cinnamon, allspice, salt, pepper)',
+        'Tomato sauce or diced tomatoes','Lemon (optional)'],
+
+        '1. Prepare grape leaves by rinsing them and blanching in hot water until they are pliable.\n',
+        '2. In a bowl, mix ground meat (beef or lamb) with rice, finely chopped onions, and a blend of spices.\n',
+        '3. Place a grape leaf flat on a surface, add a spoonful of the meat mixture at the center of the leaf.\n',
+        '4. Fold the sides of the grape leaf over the filling and roll it tightly, creating a compact cylinder.\n',
+        '5. Repeat the process until all the grape leaves are filled and rolled.\n',
+        '6. Line the bottom of a pot with additional grape leaves to prevent sticking.\n',
+        '7. Arrange the stuffed grape leaves in layers in the pot.\n',
+        '8. Mix tomato sauce with water and pour it over the stuffed grape leaves in the pot.\n',
+        '9. Place a heavy plate or lid on top to keep the yabraq in place during cooking.\n',
+        '10. Simmer on low heat for 45-60 minutes or until the rice is cooked and the grape leaves are tender.\n',
+        '11. Serve the Syrian Yabraq hot, optionally with a side of yogurt or a squeeze of lemon.\n',
+        '12. Enjoy this delicious and savory Syrian dish!\n'
     ],
-    [
-        'Orman-Blaban',
-        ['Spices', 'Meat', 'Butter', 'Yogurt', 'Corn Starch', 'Egges'],
-        '- First, you should do this ...\n- Then, you can do that.\n- Finally, you are done!'
-    ]
+
 ]).
